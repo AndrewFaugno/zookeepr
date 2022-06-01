40f18c2d5ea3737c54e56f972e0cb31e77b89d58
Andrew Faugno <andrewfaugno825@gmail.com>
2022-06-01 17:29:59 -0400

    handle id request through parameter

--- server.js
+++ server.js
@@ -31,15 +31,29 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
 
 app.get('/api/animals', (req, res) => {
-    let results = animals; 
+    let result = animals; 
     if (req.query) {
-        results = filterByQuery(req.query, results);
+        result = filterByQuery(req.query, result);
     }
-    res.json(results);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    res.json(result);
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
